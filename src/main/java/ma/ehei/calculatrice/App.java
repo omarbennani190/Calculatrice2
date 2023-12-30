41b9b885cec7191cc50b84720c1910e885bd089f
omarbennani190 <153104871+omarbennani190@users.noreply.github.com>
2023-12-30 13:16:10 +0100

    Correction de la fonction d'addition

--- src/main/java/ma/ehei/calculatrice/App.java
+++ src/main/java/ma/ehei/calculatrice/App.java
@@ -2,9 +2,9 @@ package ma.ehei.calculatrice;
 
 public class App {
 	public static void main(String[] args) {
-		System.out.println(Calculatrice.add(5, 5));
+		System.out.println("Résultat de l'addition : " + Calculatrice.add(5, 5));
 		
-        //System.out.println("Résultat de la multiplication : " + Calculatrice.multiplier(2, 4));
+        System.out.println("Résultat de la multiplication : " + Calculatrice.multiplier(2, 4));
     
 	}
 }
