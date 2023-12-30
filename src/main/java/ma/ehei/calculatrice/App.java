196b5d6814ab4cedb95e608a045cc47460ada58c
omarbennani190 <153104871+omarbennani190@users.noreply.github.com>
2023-12-30 13:04:40 +0100

    Initialisation du projet calculatrice

---
+++ src/main/java/ma/ehei/calculatrice/App.java
@@ -0,0 +1,10 @@
+package ma.ehei.calculatrice;
+
+public class App {
+	public static void main(String[] args) {
+		System.out.println(Calculatrice.add(5, 5));
+		
+        //System.out.println("Résultat de la multiplication : " + Calculatrice.multiplier(2, 4));
+    
+	}
+}
---
+++ src/main/java/ma/ehei/calculatrice/Calculatrice.java
@@ -0,0 +1,10 @@
+package ma.ehei.calculatrice;
+
+public class Calculatrice {
+	public static int add(int a, int b) {
+		return a+b; // à corriger par a+b
+	}
+	//public int multiplier(int a, int b) {
+        //return a * b;
+    //}
+}
